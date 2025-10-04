5a06ffbba6f1f6bef42c3c616df79a9f6689cd31
pandoras <pandoras@edu.21-school.ru>
2025-10-04 17:25:15 +0700

    Not working version with XmlField annotation and Person test class

--- src/main/java/org/example/Person.java
+++ src/main/java/org/example/Person.java
@@ -1,4 +1,25 @@
 package org.example;
 
 public class Person {
+    @XmlField
+    private String name;
+
+    @XmlField(name = "years")
+    private int age;
+
+    @XmlField
+    private double height;
+
+    public Person(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public Person() {}
+
+    @Override
+    public String toString() {
+        return "Person{name='" + name + "', age=" + age + ", height=" + height + "}";
+    }
 }
--- src/main/java/org/example/XmlField.java
+++ src/main/java/org/example/XmlField.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public interface XmlField {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface XmlField {
+    String name() default "";
 }
